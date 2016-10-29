a1c18b62433d481a5b04f9540c17a7be77c90b9c
Nicole <nicole@nicoledonnelly.me>
2016-10-29 13:23:31 -0400

    added about, contact, layouts. updated navigation

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
-   url(r'^$', 'collection.views.index', name='home'),
-   url(r'^admin/', admin.site.urls),
+    url(r'^$', 'collection.views.index', name='home'),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
+    url(r'^admin/', admin.site.urls),
 ]
